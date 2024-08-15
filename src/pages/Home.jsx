d54dbc871f6f0818816adf970c1a51735289803b
fayshal69 <foyshalbinamir@gmail.com>
2024-08-15 15:03:10 +0600

    add: filter all func

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,11 @@ const Home = () => {
     const [priceSorting, setPriceSorting] = useState('');
     const [dateSorting, setDateSorting] = useState(false);
 
-    console.log(priceSorting);
+    const filterData = {
+        search, brand, category, priceRange, priceSorting, dateSorting
+    }
+
+    // console.log(priceRange);
 
     const handleSearch = (e) => {
         e.preventDefault();
@@ -30,14 +34,14 @@ const Home = () => {
 
     useEffect(() => {
         setLoading(true);
-        fetch(`${import.meta.env.VITE_BASE_URL}/products?page=${currentPage - 1}&search=${search}&brand=${brand}`)
+        fetch(`${import.meta.env.VITE_BASE_URL}/products?page=${currentPage - 1}&filterData=${JSON.stringify(filterData)}`)
             .then((res) => res.json())
             .then((data) => {
                 setProducts(data?.result);
                 setTotalProducts(Number(data?.total));
                 setLoading(false);
             })
-    }, [search, currentPage, brand])
+    }, [search, currentPage, brand, category, priceRange, priceSorting, dateSorting, totalProducts])
 
     useEffect(() => {
         setLoading(true);
@@ -49,22 +53,23 @@ const Home = () => {
                 setLoading(false);
             })
     }, [])
-
+    
     const pages = [...Array(Math.ceil(parseInt(totalProducts) / 12)).keys()];
 
     const priceRanges = [
         { price: "0 - 499" },
         { price: "500 - 999" },
         { price: "1000 - 1999" },
-        { price: "2000 - 5000" },
+        { price: "2000 - 5000" }
     ]
 
     return (
         <div className="py-6 md:py-8 lg:py-12">
+            {/* search form */}
             <div>
                 <form onSubmit={handleSearch} className="flex justify-center mb-6">
                     <label className="input input-bordered flex items-center gap-2 w-full lg:w-1/2 mx-auto">
-                        <input type="text" name="search" className="grow w-full" placeholder="Search product name" />
+                        <input type="search" name="search" className="grow w-full" placeholder="Search product name" />
                         <button type="submit" className="bg-sky-200 rounded-full px-3 py-2">Search</button>
                     </label>
                 </form>
@@ -79,54 +84,58 @@ const Home = () => {
                 <div className="drawer-side">
                     <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
                     <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-                        {/* brand selection */}
-                        <div className="mb-5">
-                            <p className="font-medium text-lg border-b mb-3">Brand</p>
-                            <form>
-                                {
-                                    brands && brands.map((b) => <div key={b._id} className="flex items-center gap-2 mb-1"><input type="checkbox" className="checkbox" onChange={(e) => e.target.checked ? setBrand([...brand, b?._id]) :
-                                        setBrand(brand.filter((r) => r !== b?._id))} /> <span>{b._id}</span></div>)
-                                }
-                            </form>
-                        </div>
-                        {/* category selection */}
-                        <div className="mb-5">
-                            <p className="font-medium text-lg border-b mb-3">Category</p>
-                            <form>
-                                {
-                                    categories && categories.map((c) => <div key={c._id} className="flex items-center gap-2 mb-1"><input type="checkbox" className="checkbox" onChange={(e) => e.target.checked ? setCategory([...category, c._id]) : setCategory(category.filter((r) => r !== c?._id))} /> <span>{c._id}</span></div>)
-                                }
-                            </form>
-                        </div>
-                        {/* price range selection */}
-                        <div className="mb-5">
-                            <p className="font-medium text-lg border-b mb-3">Price Range</p>
-                            <form>
-                                {
-                                    priceRanges.map((price) => <div key={price.price} className="flex items-center gap-2 mb-1"><input type="checkbox" className="checkbox" onChange={(e) => e.target.checked ? setPriceRange([...priceRange, price.price]) : setPriceRange(priceRange.filter((r) => r !== price.price))} /> <span>{price.price}</span></div>)
-                                }
-                            </form>
-                        </div>
-                        {/* price sorting selection */}
-                        <div className="mb-5">
-                            <p className="font-medium text-lg border-b mb-3">Price Sorting</p>
-                            <form>
+                        <form onChange={() => setCurrentPage(1)}>
+                            {/* brand selection */}
+                            <div className="mb-5">
+                                <p className="font-medium text-lg border-b mb-3">Brand</p>
+                                <div>
+                                    {
+                                        brands && brands.map((b) => <div key={b._id} className="flex items-center gap-2 mb-1"><input type="checkbox" className="checkbox" onChange={(e) => e.target.checked ? setBrand([...brand, b?._id]) :
+                                            setBrand(brand.filter((r) => r !== b?._id))} /> <span>{b._id}</span></div>)
+                                    }
+                                </div>
+                            </div>
+                            {/* category selection */}
+                            <div className="mb-5">
+                                <p className="font-medium text-lg border-b mb-3">Category</p>
+                                <div>
+                                    {
+                                        categories && categories.map((c) => <div key={c._id} className="flex items-center gap-2 mb-1"><input type="checkbox" className="checkbox" onChange={(e) => e.target.checked ? setCategory([...category, c._id]) : setCategory(category.filter((r) => r !== c?._id))} /> <span>{c._id}</span></div>)
+                                    }
+                                </div>
+                            </div>
+                            {/* price range selection */}
+                            <div className="mb-5">
+                                <p className="font-medium text-lg border-b mb-3">Price Range</p>
+                                <div>
+                                    {
+                                        priceRanges.map((price) => <div key={price.price} className="flex items-center gap-2 mb-1"><input type="radio" name="radio-1" className="radio" onChange={(e) => e.target.checked ? setPriceRange(price.price.split("-")) : setPriceRange('')} /> <span>{price.price}</span></div>)
 
-                                <div className="flex items-center gap-2 mb-1"><input type="radio" className="radio" name="radio-1" onChange={(e) => e.target.checked ? setPriceSorting('lth') : setPriceSorting("")} /> <span>{"Low to High"}</span></div>
-                                <div className="flex items-center gap-2 mb-1"><input type="radio" className="radio" name="radio-1" onChange={(e) => e.target.checked ? setPriceSorting('htl') : setPriceSorting("")} /> <span>{"High to Low"}</span></div>
-                                <div className="flex items-center gap-2 mb-1"><input type="radio" className="radio" name="radio-1" onChange={(e) => e.target.checked ? setPriceSorting('') : setPriceSorting("")} /> <span>{"None"}</span></div>
+                                    }
+                                    <div className="flex items-center gap-2 mb-1"><input type="radio" name="radio-1" className="radio" onChange={(e) => e.target.checked ? setPriceRange([]) : setPriceRange([])} /> <span>None</span></div>
+                                </div>
+                            </div>
+                            {/* price sorting selection */}
+                            <div className="mb-5">
+                                <p className="font-medium text-lg border-b mb-3">Price Sorting</p>
+                                <div>
 
-                            </form>
-                        </div>
-                        {/* date sorting selection */}
-                        <div className="mb-5">
-                            <p className="font-medium text-lg border-b mb-3">Date Sorting</p>
-                            <form>
+                                    <div className="flex items-center gap-2 mb-1"><input type="radio" className="radio" name="radio-1" onChange={(e) => e.target.checked ? setPriceSorting('lth') : setPriceSorting("")} /> <span>{"Low to High"}</span></div>
+                                    <div className="flex items-center gap-2 mb-1"><input type="radio" className="radio" name="radio-1" onChange={(e) => e.target.checked ? setPriceSorting('htl') : setPriceSorting("")} /> <span>{"High to Low"}</span></div>
+                                    <div className="flex items-center gap-2 mb-1"><input type="radio" className="radio" name="radio-1" onChange={(e) => e.target.checked ? setPriceSorting('') : setPriceSorting("")} /> <span>{"None"}</span></div>
 
-                                <div className="flex items-center gap-2 mb-1"><input type="checkbox" className="checkbox" onChange={(e) => e.target.checked ? setDateSorting(true) : setDateSorting(false)} /> <span>{"Show the newest"}</span></div>
+                                </div>
+                            </div>
+                            {/* date sorting selection */}
+                            <div className="mb-5">
+                                <p className="font-medium text-lg border-b mb-3">Date Sorting</p>
+                                <div>
 
-                            </form>
-                        </div>
+                                    <div className="flex items-center gap-2 mb-1"><input type="checkbox" className="checkbox" onChange={(e) => e.target.checked ? setDateSorting(true) : setDateSorting(false)} /> <span>{"Show the newest"}</span></div>
+
+                                </div>
+                            </div>
+                        </form>
 
                     </ul>
                 </div>
@@ -137,13 +146,12 @@ const Home = () => {
 
             {
                 loading ? <LoadingSpinner></LoadingSpinner> :
-                    products ? <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6">
 
                         {
                             products && products.map((product) => <ProductCard key={product._id} product={product}></ProductCard>)
                         }
-                    </div> :
-                        <h3>hii</h3>
+                    </div>
             }
 
             <div className="mt-6 md:mt-8">
